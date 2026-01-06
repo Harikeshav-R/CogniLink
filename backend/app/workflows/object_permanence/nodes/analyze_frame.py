33a13a9770827959ccc900066337a5a196de36e4
Harikeshav R <r.harikeshav@gmail.com>
2026-01-05 23:14:28 -0500

    refactor(analyze_frame): update message structure and refine response handling
    - Change `HumanMessage` to a dictionary format adhering to updated agent message structure.
    - Adjust image encoding type from `jpeg` to `png` for compatibility.
    - Simplify response processing using `structured_response` attribute.
    - Remove unused `HumanMessage` and commented-out model initialization code.

--- backend/app/workflows/object_permanence/nodes/analyze_frame.py
+++ backend/app/workflows/object_permanence/nodes/analyze_frame.py
@@ -1,5 +1,5 @@
 from langchain.agents import create_agent
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage
 from loguru import logger
 
 from app.core.config import Config
@@ -17,6 +17,11 @@ async def analyze_frame(state: ObjectPermanenceWorkflowState):
         Config.POLLINATIONS_API_KEY
     )
 
+    # model = init_google_genai_chat_model(
+    #     Config.GEMINI_FAST_MODEL,
+    #     Config.GEMINI_API_KEY
+    # )
+
     agent = create_agent(
         model=model,
         response_format=FrameAnalysis,
@@ -25,14 +30,21 @@ async def analyze_frame(state: ObjectPermanenceWorkflowState):
         ),
     )
 
-    response: FrameAnalysis = await agent.ainvoke(
-        HumanMessage(
-            content=[
-                {"type": "text", "text": "Here is your frame to analyze:"},
-                {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{state.current_frame_b64}"}},
+    response = await agent.ainvoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "Here is your frame to analyze:"},
+                        {"type": "image_url",
+                         "image_url": {"url": f"data:image/png;base64,{state.current_frame_b64}"}},
+                    ]
+                }
             ]
-        )
+        }
     )
+    response: FrameAnalysis = response["structured_response"]
 
     logger.info(f"Detected Room: {response.scene.room_name}")
     logger.info(f"Detected {len(response.objects)} objects.")
