fa095a6957536617bfd8ba615d9b29c49c24418b
Harikeshav R <r.harikeshav@gmail.com>
2026-01-05 21:20:14 -0500

    feat(workflows): add `analyze_frame` node for object permanence workflow
    - Implement `analyze_frame` function to process and analyze video frames using Pollinations model and LangChain agents.
    - Integrate base64-encoded frame analysis and structured responses with the `FrameAnalysis` schema.
    - Enhance logging to track detected rooms and objects during frame analysis.

---
+++ backend/app/workflows/object_permanence/nodes/analyze_frame.py
@@ -0,0 +1,40 @@
+from langchain.agents import create_agent
+from langchain_core.messages import SystemMessage, HumanMessage
+from loguru import logger
+
+from app.core.config import Config
+from app.shared.model_factory import init_pollinations_chat_model
+from app.workflows.object_permanence.prompts import Prompts
+from app.workflows.object_permanence.schemas import FrameAnalysis, ObjectPermanenceWorkflowState
+
+
+async def analyze_frame(state: ObjectPermanenceWorkflowState):
+    if not state.current_frame_b64:
+        return {}
+
+    model = init_pollinations_chat_model(
+        Config.POLLINATIONS_VISION_MODEL,
+        Config.POLLINATIONS_API_KEY
+    )
+
+    agent = create_agent(
+        model=model,
+        response_format=FrameAnalysis,
+        system_prompt=SystemMessage(
+            content=Prompts.ANALYZE_FRAME
+        ),
+    )
+
+    response: FrameAnalysis = await agent.ainvoke(
+        HumanMessage(
+            content=[
+                {"type": "text", "text": "Here is your frame to analyze:"},
+                {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{state.current_frame_b64}"}},
+            ]
+        )
+    )
+
+    logger.info(f"Detected Room: {response.scene.room_name}")
+    logger.info(f"Detected {len(response.objects)} objects.")
+
+    return {"current_analysis": response}
