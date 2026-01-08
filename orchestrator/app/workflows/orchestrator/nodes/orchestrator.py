2d422f7e76c3920c1bb19efb67ec57d8f331ddf9
Harikeshav R <r.harikeshav@gmail.com>
2026-01-07 19:36:20 -0500

    feat(workflows): add `orchestrator` node to handle workflow selection via LLM agent
    - Implement `orchestrator` function to process user queries and select appropriate workflows using `langchain` agents.
    - Integrate `Prompts.GENERATE_ANSWER` and `SelectedWorkflow` schema for structured response generation.

---
+++ orchestrator/app/workflows/orchestrator/nodes/orchestrator.py
@@ -0,0 +1,37 @@
+from langchain.agents import create_agent
+from langchain_core.messages import SystemMessage
+
+from app.core.config import Config
+from app.shared.model_factory import init_pollinations_chat_model
+from app.workflows.object_permanence.prompts import Prompts
+from app.workflows.orchestrator.schemas import OrchestratorWorkflowState, SelectedWorkflow
+
+
+async def orchestrator(state: OrchestratorWorkflowState) -> dict:
+    model = init_pollinations_chat_model(
+        Config.POLLINATIONS_FAST_MODEL,
+        Config.POLLINATIONS_API_KEY
+    )
+
+    agent = create_agent(
+        model=model,
+        response_format=SelectedWorkflow,
+        system_prompt=SystemMessage(
+            content=Prompts.GENERATE_ANSWER
+        ),
+    )
+    response = await agent.ainvoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": state.query}
+                    ]
+                }
+            ]
+        }
+    )
+
+    structured_response: SelectedWorkflow = response["structured_response"]
+    return {"selected_workflow": structured_response}
