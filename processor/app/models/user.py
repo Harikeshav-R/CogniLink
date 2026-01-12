945d7a9be63ecd64988da7fce9e3df427a26a352
Harikeshav R <r.harikeshav@gmail.com>
2026-01-12 16:52:20 -0500

    feat(models): add `User` and `Person` models with relations
    - Introduced `User` model to represent application users, including face embedding field using pgvector.
    - Added `Person` model to define related people with fields for name, birthdate, and relationship to the user.
    - Established one-to-many relationship between `User` and `Person`.

---
+++ processor/app/models/user.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from pgvector.sqlalchemy import Vector
+from sqlmodel import SQLModel, Field, Column, Relationship
+
+from app.models.person import Person
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True, description="The primary key of the table.")
+    first_name: str = Field(index=True, description="The first name of the user.")
+    last_name: str = Field(index=True, description="The last name of the user.")
+    face_embedding: Vector = Field(
+        sa_column=Column(Vector(4096)), description="The embedding of the user."
+    )
+
+    # Relationship: One user can have many related people
+    related_people: list[Person] = Relationship(back_populates="user")
